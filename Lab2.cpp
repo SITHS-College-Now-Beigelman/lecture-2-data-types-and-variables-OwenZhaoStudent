a4642db4e92c0f5b77c3183b31d9f9cd9c0072a2
OwenZhaoStudent <owenz19@nycstudents.net>
2024-09-17 15:30:38 -0400

    9/17/24 Lecture 2

--- Lab2.cpp
+++ Lab2.cpp
@@ -27,11 +27,20 @@ int main()
     //Comparing Characters
     char Char1 = 'A';
     char Char2 = 'B';
+    bool Compare2 = Char1 > Char2;
+
+    //Compare and Print the Characters
+    cout << "Character 1 is A and Character 2 is B. Is Character 1 greater than Character 2? True = 1 False = 0    Answer: " << Compare2 << endl << endl;
 
     //Creating Variables for Arithmetic Expressions
     double Expression1 = 2 + 3.5;
-    double Expression2 = 6 / 4 + 3.9;
-    double Expression3 = 5.4 * 2 - (13.6 + 18 / 2);
+    double Thing1 = 6 / 4;
+    double Thing2 = 3.9;
+    double Expression2 = Thing1 + Thing2;
+    double Thing3 = 18 / 2;
+    double Thing4 = 5.4 * 2;
+    double Thing5 = 13.6 + Thing3;
+    double Expression3 = Thing4 - Thing5;
 
     //Printing Expression and Answers
     cout << "2 + 3.5 = " << Expression1 << endl;
@@ -40,3 +49,17 @@ int main()
 
     return 0;
 }
+
+/*Output:
+int: 6
+float: 1.68
+double: 6.58
+char: A
+bool: 0
+string: Chicken
+
+Character 1 is A and Character 2 is B. Is Character 1 greater than Character 2? True = 1 False = 0    Answer: 0
+
+2 + 3.5 = 5.5
+6 / 4 + 3.9 = 4.9
+5.4 * 2 - (13.6 + 18 / 2) = -11.8*/
